283c44c6e0a4c0ac78d2c347ab362ccae0aff807
Cao Jiafeng <funfriendcjf@gmail.com>
2014-10-10 17:30:03 +0800

    use Show instead of ToString, and add Concatenate

--- src/regex_pattern/mod.rs
+++ src/regex_pattern/mod.rs
@@ -1,26 +1,29 @@
-pub trait Pattern: ToString {
+use std::fmt;
+use std::fmt::Show;
+
+
+pub trait Pattern: Show {
   fn precedence(&self) -> uint;
 
   fn bracket(&self, outer_precedence: uint) -> String {
     if self.precedence() < outer_precedence {
-      format!("({})", self.to_string())
+      format!("({})", self)
     } else {
-      self.to_string()
+      format!("{}", self)
     }
   }
 
   fn inspect(&self) -> String {
-    format!("/{}/", self.to_string())
+    format!("/{}/", self)
   }
 }
 
-#[deriving(Show)]
+
 pub struct Empty;
 
-impl Empty {
-  // override the ToString
-  fn to_string(&self) -> String {
-    "".to_string()
+impl Show for Empty {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "")
   }
 }
 
@@ -28,14 +31,14 @@ impl Pattern for Empty {
   fn precedence(&self) -> uint { 3 }
 }
 
-#[deriving(Show)]
+
 pub struct Literal {
   pub character: char
 }
 
-impl Literal {
-  fn to_string(&self) -> String {
-    String::from_char(1, self.character)
+impl Show for Literal {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    self.character.fmt(f)
   }
 }
 
@@ -43,11 +46,33 @@ impl Pattern for Literal {
   fn precedence(&self) -> uint { 3 }
 }
 
+
+/// Concatenate node
+pub struct Concatenate<S, T> {
+  pub first: S,
+  pub second: T
+}
+
+/// implement Show trait for Concatenate
+impl<S: Show, T: Show> Show for Concatenate<S, T> {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{}|{}", self.first, self.second)
+  }
+}
+
+impl<S, T> Pattern for Concatenate<S, T>
+  where S: Show + Pattern, T: Show + Pattern {
+  fn precedence(&self) -> uint { 1 }
+}
+
+
+
 #[cfg(test)]
 mod test {
   use super::Pattern;
   use super::Empty;
   use super::Literal;
+  use super::Concatenate;
 
   #[test]
   fn test_empty() {
@@ -61,5 +86,14 @@ mod test {
 
     assert_eq!(literal.precedence(), 3);
     assert_eq!(literal.to_string().as_slice(), "a");
+  }
+
+  #[test]
+  fn test_concatenate() {
+    let literal = Literal { character: 'a' };
+    let concat = Concatenate { first: Empty, second: literal };
+
+    assert_eq!(concat.precedence(), 1);
+    assert_eq!(concat.to_string().as_slice(), "|a");
   }
 }
